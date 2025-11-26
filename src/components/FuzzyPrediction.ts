c616d1028dfab5b5d64e7272cd89c12651b3740e
Viktor <viktor.avgn@yandex.ru>
2025-11-26 23:34:35 +0700

    FP скорректирован для сборщика

--- src/components/FuzzyPrediction.ts
+++ src/components/FuzzyPrediction.ts
@@ -1,18 +1,22 @@
-
-import type { OilPricePoint } from '../types';
-import { songChissomForecast } from '../lib/songChissom';
+import type { OilPricePoint } from '../types'
+import { songChissomForecast } from '../lib/songChissom'
 
 export const applySongChissomPredictionWithParams = (
-   OilPricePoint[],
+  data: OilPricePoint[],
   universeMin: number,
   universeMax: number,
   numFuzzySets: number
 ): OilPricePoint[] => {
-  const actuals = data.map((d) => d.actual);
-  const forecasts = songChissomForecast(actuals, universeMin, universeMax, numFuzzySets);
+  const actuals = data.map((d) => d.actual)
+  const forecasts = songChissomForecast(
+    actuals,
+    universeMin,
+    universeMax,
+    numFuzzySets
+  )
 
   return data.map((point, index) => ({
     ...point,
     predicted: index === 0 ? undefined : forecasts[index - 1],
-  }));
-};
+  }))
+}
